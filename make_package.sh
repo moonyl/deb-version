fee7d6e9174c15cf824ca90d9ccc097a38613b53
Lee Sang Moon <moonysl@hanmail.net>
2023-11-10 17:47:44 +0900

    package생성용 스크립트 추가.

---
+++ make_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# gpg --list-signatures 실행 결과
+gpg_output=$(gpg --list-signatures)
+
+# 특정 키 ID를 추출하기 위해 grep 사용
+key_id=$(echo "$gpg_output" | grep -oP 'sig \d+\s+\K[0-9A-F]+')
+
+# 추출된 키 ID 출력
+echo "Key ID: $key_id"
+
+dpkg-sig -k $key_id --sign posod cpack-study_1.0.3-test_amd64.deb
